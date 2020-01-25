228bb6a1568e1256ce54e6033f6eb67c89bd12bc
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-01-25 02:37:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves) #code an argument here
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls = ["earth" , "wind" , "fire" , "water , "]
+  planeteer_calls = ["earth" , "wind" , "fire" , "water" , "heart"]
+  planeteer_calls.map { |call| call.capitalize
 end
 
 
